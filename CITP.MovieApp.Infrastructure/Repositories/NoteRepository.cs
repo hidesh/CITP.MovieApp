d0e6504f4048d3e4cc416268e9ff6ac60e854cd4
Camille Kirstine <charlieslarsson@icloud.com>
2025-10-29 14:31:37 +0100

    Started setting up NoteRepository

---
+++ CITP.MovieApp.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,11 @@
+using CITP.MovieApp.Application.Abstractions;
+using CITP.MovieApp.Application.DTOs;
+using CITP.MovieApp.Domain.Entities;
+using CITP.MovieApp.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CITP.MovieApp.Infrastructure.Repositories
+{
+ 
+}
+
