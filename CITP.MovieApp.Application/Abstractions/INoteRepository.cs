f16bae5baf4eb8df77c930882198d3d0f647e781
Camille Kirstine <charlieslarsson@icloud.com>
2025-10-29 14:11:00 +0100

    Set up preliminary interface for NoteRepository

--- CITP.MovieApp.Application/Abstractions/INoteRepository.cs
+++ CITP.MovieApp.Application/Abstractions/INoteRepository.cs
@@ -1,6 +1,13 @@
-namespace CITP.MovieApp.Application.Abstractions;
+using CITP.MovieApp.Application.DTOs;
 
-public class INoteRepository
+namespace CITP.MovieApp.Application.Abstractions
 {
-    
+    public interface INoteRepository
+    {
+        Task<IEnumerable<NoteDto>> GetAllForUserAsync(int userId, string? tconst = null, string? nconst = null);
+        Task<NoteDto?> GetForUserByIdAsync(int noteId, int userId);
+        Task<int> CreateAsync(int userId, NoteCreateDto dto);
+        Task<bool> UpdateAsync(int noteId, int userId, NoteUpdateDto dto);
+        Task<bool> DeleteAsync(int noteId, int userId);
+    }
 }
