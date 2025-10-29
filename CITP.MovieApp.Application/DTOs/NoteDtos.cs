95802a08bbe73a6bef2d990e30b656ea850d6ef4
Camille Kirstine <charlieslarsson@icloud.com>
2025-10-29 14:07:15 +0100

    DTOs for Note Creation and Note Updates

--- CITP.MovieApp.Application/DTOs/NoteDtos.cs
+++ CITP.MovieApp.Application/DTOs/NoteDtos.cs
@@ -1,12 +1,27 @@
-namespace CITP.MovieApp.Application.DTOs;
-
-public class NoteDto
+namespace CITP.MovieApp.Application.DTOs
 {
-    public int NoteId { get; set; }
-    public int UserId { get; set; }
-    public string? Tconst { get; set; }
-    public string? Nconst { get; set; }
-    public string Content { get; set; } = null!;
-    public DateTime NotedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+
+    public class NoteDto
+    {
+        public int NoteId { get; set; }
+        public int UserId { get; set; }
+        public string? Tconst { get; set; }
+        public string? Nconst { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime NotedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public class NoteCreateDto
+    {
+        public string? Tconst { get; set; }
+        public string? Nconst { get; set; }
+        public string Content { get; set; } = null!;
+    }
+    
+    public class NoteUpdateDto
+    {
+        public string Content { get; set; } = null!;
+    }
+    
 }
