12d78b4993a28f04de62527a9c4351cd6925ef0e
Camille Kirstine <charlieslarsson@icloud.com>
2025-11-03 16:55:04 +0100

    added virutal methods to UserRepository to be able to make mock repository for unit tests

--- CITP.MovieApp.Infrastructure/Repositories/UserRepository.cs
+++ CITP.MovieApp.Infrastructure/Repositories/UserRepository.cs
@@ -11,22 +11,22 @@ namespace CITP.MovieApp.Infrastructure.Repositories
         private readonly AppDbContext _context;
         public UserRepository(AppDbContext context) => _context = context;
 
-        public async Task<User?> GetByUsernameAsync(string username)
+        public virtual async Task<User?> GetByUsernameAsync(string username)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
         }
 
-        public async Task<User?> GetByEmailAsync(string email)
+        public virtual async Task<User?> GetByEmailAsync(string email)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
 
-        public async Task AddAsync(User user)
+        public virtual async Task AddAsync(User user)
         {
             await _context.Users.AddAsync(user);
         }
 
-        public async Task SaveAsync()
+        public virtual async Task SaveAsync()
         {
             await _context.SaveChangesAsync();
         }
