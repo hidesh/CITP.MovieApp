c1af3981a697e5968b2de931964758950b796344
Camille Kirstine <charlieslarsson@icloud.com>
2025-11-03 16:38:00 +0100

    Started unit tests for AuthController

---
+++ CITP.MovieApp.Tests/UnitTests/AuthControllerTests.cs
@@ -0,0 +1,17 @@
+using CITP.MovieApp.Api.Controllers;
+using CITP.MovieApp.Application.DTOs;
+using CITP.MovieApp.Domain.Entities;
+using CITP.MovieApp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace CITP.MovieApp.Tests_.UnitTests
+{
+    public class AuthControllerTests
+    {
+       
+
+        
+    }
+}
