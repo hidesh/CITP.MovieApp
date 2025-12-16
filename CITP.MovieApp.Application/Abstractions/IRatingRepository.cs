e43311bd59f75e9ca6a81e335246459cc46764a8
Camille Kirstine <charlieslarsson@icloud.com>
2025-12-16 13:20:36 +0100

    changes for ratings and updating user values

--- CITP.MovieApp.Application/Abstractions/IRatingRepository.cs
+++ CITP.MovieApp.Application/Abstractions/IRatingRepository.cs
@@ -5,10 +5,14 @@ namespace CITP.MovieApp.Application.Abstractions
     public interface IRatingRepository
     {
         Task<IEnumerable<RatingDto>> GetAllForUserAsync(int userId);
-        Task<MovieRatingsDto> GetAllByMovieAsync(string tconst); // Returns IMDb + user ratings
+        Task<MovieRatingsDto> GetAllByMovieAsync(string tconst);
         Task<IEnumerable<RatingDto>> GetAllForUserByMovieAsync(int userId, string tconst);
+
         Task<int> CreateForMovieAsync(int userId, string tconst, RatingCreateDto dto);
         Task<bool> UpdateAsync(int ratingId, int userId, RatingUpdateDto dto);
         Task<bool> DeleteAsync(int ratingId, int userId);
+        
+        Task<int> CreateOrUpdateForMovieAsync(int userId, string tconst, RatingCreateDto dto);
     }
+
 }
